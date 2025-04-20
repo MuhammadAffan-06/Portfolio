aa8c031bd30b4c3ab5ae9f8c41050e9fee47c411
MuhammadAffan-06 <maffan2202@gmail.com>
2025-04-20 21:46:21 +0500

    added tech stack

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/ui/navbar/navbar";
 import Introduction from "@/components/ui/introduction/introduction";
 import StarryBackground from "@/components/ui/starry-background/stars";
 import TechStack from "@/components/ui/techstack/tech-stack";
-import Developments from "@/components/ui/developments/developments";
+// import Developments from "@/components/ui/developments/developments";
 export default function Home() {
   return (
     <main className="relative w-full min-h-screen overflow-hidden">
